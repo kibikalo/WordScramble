8a5e1b9a0bd07ee746431cf0acb4621f98227e22
Bohdan Kibkalo <bohda.kibkalo@gmail.com>
2025-10-15 18:21:34 +0200

    100 Days of SwiftUI - Days 31

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -10,12 +10,18 @@ struct ContentView: View {
     @State private var errorMessage = ""
     @State private var showingError = false
     
+    @State private var score = 0
+    @State private var streak = 0
+    
+    @State private var isAnimating = false
+    
     var body: some View {
         NavigationStack {
             List {
                 Section {
-                    TextField("Enter your word", text: $newWord)
+                    TextField("Form new words from the root", text: $newWord)
                         .textInputAutocapitalization(.never)
+                        .autocorrectionDisabled(true)
                 }
                 .onSubmit(addNewWord)
                 
@@ -26,42 +32,60 @@ struct ContentView: View {
                             Image(systemName: "\(word.count).circle")
                             Text(word)
                         }
-                        
+                        .transition(.asymmetric(insertion: .move(edge: .top).combined(with: .opacity),
+                                                removal: .opacity))
                     }
                 }
             }
-            .navigationTitle(rootWord)
+            .safeAreaPadding(.top, 20)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    VStack (spacing: 10){
+                        Spacer()
+                        Spacer()
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text(rootWord).font(.largeTitle)
+                            HStack {
+                                Text("Words: \(usedWords.count) ")
+                                    .font(.callout).foregroundStyle(.secondary)
+                                    .animation(.easeInOut(duration: 0.5), value: usedWords.count)
+                                Text("Score: \(score)")
+                                    .font(.callout).foregroundStyle(.secondary)
+                                    .animation(.easeInOut(duration: 0.5), value: score)
+                                Text("Streak: \(streak)")
+                                    .font(.callout).foregroundStyle(.secondary)
+                                    .animation(.easeInOut(duration: 0.5), value: streak)
+                            }
+                        }
+                    }
+                }
+                
+                ToolbarItem(placement: .bottomBar) {
+                    ZStack {
+                        Circle ()
+                            .fill(AngularGradient(colors: [.teal, .indigo, .blue], center: .center, angle: .degrees(isAnimating ? 360 : 0)))
+                            .blur(radius: 10)
+                            .frame(width: 50, height: 50)
+                            .onAppear {
+                                withAnimation(Animation.linear(duration: 5).repeatForever(autoreverses: false)) {
+                                    isAnimating = true
+                                }
+                            }
+                        Button(action: {
+                            startGame()
+                        }) {
+                            Image(systemName: "arrow.clockwise")
+                        }
+                    }
+                }
+            }
             .onAppear(perform: startGame)
             .alert(errorTitle, isPresented: $showingError) { } message: {
                 Text(errorMessage)
             }
         }
-    }
-    
-    func addNewWord() {
-        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         
-        guard answer.count > 0 else { return }
-        
-        guard isOriginal(word: answer) else {
-            wordError(title: "Word used already", message: "Be more original")
-            return
-        }
-        
-        guard isPossible(word: answer) else {
-            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
-            return
-        }
-        
-        guard isReal(word: answer) else {
-            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
-            return
-        }
-        
-        withAnimation {
-            usedWords.insert(answer, at: 0)
-        }
-        newWord = ""
     }
     
     func startGame() {
@@ -78,9 +102,68 @@ struct ContentView: View {
         fatalError("Could not load start.txt from bundle.")
     }
     
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard answer.count > 0 else { return }
+        
+        guard isTooShort(word: answer) else {
+            wordError(title: "Word is too short", message: "Write something longer than 2 letters")
+            return
+        }
+        
+        guard isNotSameAsRoot(word: answer) else {
+            wordError(title: "Word is the same as the root", message: "Be smarter than that!")
+            return
+        }
+        
+        
+        guard isOriginal(word: answer) else {
+            streak = 0
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            streak = 0
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            streak = 0
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+        
+        streak += 1
+        score += calculateScore(answer)
+        
+        withAnimation(.spring(response: 0.35, dampingFraction: 0.7)) {
+            usedWords.insert(answer, at: 0)
+        }
+        
+        newWord = ""
+    }
+    
+    func calculateScore(_ word: String) -> Int {
+        return 10 * word.count * streak
+    }
+    
+    
+    // Validation Func
+    func isTooShort(word: String) -> Bool {
+        word.count > 2
+    }
+    
+    func isNotSameAsRoot(word: String) -> Bool {
+        word.lowercased() != rootWord.lowercased()
+    }
+    
     func isOriginal(word: String) -> Bool {
         !usedWords.contains(word)
     }
+    
     
     func isPossible(word: String) -> Bool {
         var tempWord = rootWord
@@ -104,6 +187,7 @@ struct ContentView: View {
         return misspelledRange.location == NSNotFound
     }
     
+    // Error handling
     func wordError(title: String, message: String) {
         errorTitle = title
         errorMessage = message
