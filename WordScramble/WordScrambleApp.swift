90ec767967717a6ace03ecbbd9bcc994cb4bcd64
Bohdan Kibkalo <bohda.kibkalo@gmail.com>
2025-10-15 16:13:55 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Bohdan on 15/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
